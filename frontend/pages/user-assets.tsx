7854e14c704ef95bcf2a0f1204e5fa4910b8b842
tomoish <tomo.i.120608@gmail.com>
2023-12-07 19:31:18 +0900

    Add carousel in user-assets

--- frontend/pages/user-assets.tsx
+++ frontend/pages/user-assets.tsx
@@ -1,38 +1,86 @@
 import Item from "@/components/items/Item";
+import Layout from "@/components/layout/Layout";
 import { ItemType } from "@/types/ItemType";
 import Image from 'next/image';
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 
 export default function Index() {
     const item: ItemType = {
-        id: 0,
-        name: "sample",
-        status: "sold",
-        url: "",
-        price: 200,
+      id: 0,
+      name: "乃木コレ7",
+      status: "sold",
+      url: "/images/乃木コレ7.jpg",
+      price: 200,
+    };
+    const item2: ItemType = {
+      id: 0,
+      name: "乃木コレ8",
+      status: "hold",
+      url: "/images/乃木コレ8.jpg",
+      price: 200,
+    };
+    const item3: ItemType = {
+      id: 0,
+      name: "乃木コレ9",
+      status: "hold",
+      url: "/images/乃木コレ9.jpg",
+      price: 200,
     };
 
     return (
-        <>
-            <div>
-                {/* 総資産推移 */}
-                <Image
-                    src="/images/資産総額推移.png" // Replace with the actual image source
-                    className="object-contain h-48 w-48 j"
-                    width={100}
-                    height={100}
-                    alt="Picture of the author"
-                />
-                {/* 期間選択ボタン */}
-            </div>
-            {/* レコメンドカード */}
-            <div className="grid grid-cols-3 gap-4">
-                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
-                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
-                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
-                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
-                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
-            </div>
-            {/* アイテム縦一覧 */}
-        </>
+      <Layout title="home">
+        <div>
+          {/* 総資産推移 */}
+          <div> 推定総資産</div>
+          <Image
+            src="/images/資産総額推移.png" // Replace with the actual image source
+            className="object-contain h-48 w-48 j"
+            width={100}
+            height={100}
+            alt="Picture of the author"
+          />
+          {/* 期間選択ボタン */}
+          <div>月間</div>
+          <div>年間</div>
+          <div>全期間</div>
+        </div>
+        {/* 人気カテゴリー */}
+        <div>1位 乃木坂</div>
+        <div>+ 300 %</div>
+        <div>2位 ポケカ</div>
+        <div>+ 120 %</div>
+        {/* アイテム縦一覧 */}
+        <div> 人気コレクション</div>
+        <Swiper
+          loop={true}
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+        >
+          <SwiperSlide>
+            <Item item={item2} showPrice={true} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Item item={item} showPrice={true} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Item item={item3} showPrice={true} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Item item={item2} showPrice={true} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Item item={item2} showPrice={true} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Item item={item2} showPrice={true} />
+          </SwiperSlide>
+        </Swiper>
+      </Layout>
     );
 }

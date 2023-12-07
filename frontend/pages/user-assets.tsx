c9fcd0338b0a98e9d79ff40477f3251a9f54762d
Reiji Kume <83393479+reiji061@users.noreply.github.com>
2023-12-07 15:56:35 +0900

    user-assetsの変更

--- frontend/pages/user-assets.tsx
+++ frontend/pages/user-assets.tsx
@@ -3,21 +3,33 @@ import Layout from "@/components/layout/Layout";
 import { ItemType } from "@/types/ItemType";
 
 export default function Index() {
-  const item: ItemType = {
-    id: 0,
-    name: "sample",
-    status: "sold",
-    url: "",
-  };
-  return (
-    <Layout title="home">
-      <div className="grid grid-cols-3 gap-4">
-        <Item item={item} />
-        <Item item={item} />
-        <Item item={item} />
-        <Item item={item} />
-        <Item item={item} />
-      </div>
-    </Layout>
-  );
+    import Image from 'next/image'; // Import the Image component from Next.js
+
+    const item: ItemType = {
+        id: 0,
+        name: "sample",
+        status: "sold",
+        url: "",
+    };
+
+    return (
+        <>
+            <div>
+                <Image
+                    src="/path/to/image.jpg" // Replace with the actual image source
+                    className="object-contain h-48 w-48"
+                    width={100}
+                    height={100}
+                    alt="Picture of the author"
+                />
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
+                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
+                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
+                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
+                <Item item={item} showPrice={true} /> {/* Add the showPrice prop */}
+            </div>
+        </>
+    );
 }

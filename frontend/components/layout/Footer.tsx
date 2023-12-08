f20de3682865b74f9f47d62e1434fe3cb1d73a05
Reiji Kume <83393479+reiji061@users.noreply.github.com>
2023-12-08 15:30:42 +0900

    footerの変更

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="bg-white flex items-center">
-      <Image
-        src="https://cdn.worldvectorlogo.com/logos/mercari-1.svg"
-        width={100}
-        height={100}
-        alt="Picture of the author"
-      />
-      <span className="ml-2">collection</span>
-      <span className="ml-2">collection</span>
-      <Link href="/user">USER PAGE</Link>
-      <Link href="/search">OTHERS PAGE</Link>
-    </div>
+    <footer className="shadow-md text-center py-4 px-4 sm:px-10 bg-white font-type1 min-h-[70px]">
+      <div className="flex flex-wrap items-center justify-between gap-5 relative">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="/user" className="hover:underline me-4 md:me-6">マイコレクション</a>
+            </li>
+            <li>
+                <a href="/search" className="hover:underline">買う</a>
+            </li>
+        </ul>
+      </div>
+    </footer>
   );
 };
 

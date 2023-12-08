6d0e178aebd8101f6bdd9a70bc93420d604199b3
tomoish <tomo.i.120608@gmail.com>
2023-12-08 11:49:26 +0900

    Change user.tsx search.tsx

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,91 +1,130 @@
 import Item from "@/components/items/Item";
 import Layout from "@/components/layout/Layout";
 import { ItemType } from "@/types/ItemType";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setItem } from "../lib/features/item/itemSlice";
 
 export default function Index() {
-  const item1: ItemType = {
+  const user_data = useSelector((state: any) => state.user.value);
+  const [items, setItems] = useState<ItemType[]>();
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch(process.env.API_URL + "/items/all")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        console.log(user_data.id);
+        const itemsList: ItemType[] = data
+          .filter((item: any) => item.user_id !== user_data.id)
+          .map((item: any) => ({
+            id: item.id,
+            name: item.name,
+            status: item.status,
+            url: item.image,
+            price: item.desired_price,
+          }));
+        setItems(itemsList);
+        console.log(items);
+      });
+  }, []);
+
+  const handleClick = (item: ItemType) => {
+    try {
+      dispatch(setItem(item));
+
+      router.push("/buy-item");
+    } catch (error) {
+      console.error("Error while fetching user data", error);
+    }
+  };
+
+  const item: ItemType = {
     id: 0,
-    name: "乃木コレ 賀喜遥香SSR",
-    status: "",
-    url: "/images/乃木コレ7_賀喜SSR.jpg",
-    price: 14000,
+    name: "乃木コレ7",
+    status: "sold",
+    url: "/images/乃木コレ7.jpg",
+    price: 200,
   };
   const item2: ItemType = {
     id: 0,
-    name: "乃木コレ 五百城茉央SR",
-    status: "sold",
-    url: "/images/乃木コレ8_五百城SR.jpg",
-    price: 2200,
+    name: "乃木コレ8",
+    status: "hold",
+    url: "/images/乃木コレ8.jpg",
+    price: 200,
   };
   const item3: ItemType = {
     id: 0,
-    name: "乃木コレ 池田瑛紗",
-    status: "",
-    url: "/images/乃木コレ9_池田1.jpg",
-    price: 580,
+    name: "乃木コレ9",
+    status: "hold",
+    url: "/images/乃木コレ9.jpg",
+    price: 200,
   };
   const item4: ItemType = {
     id: 0,
-    name: "乃木コレ 池田瑛紗",
-    status: "sold",
-    url: "/images/乃木コレ10_池田2.jpg",
-    price: 580,
+    name: "乃木コレ10",
+    status: "hold",
+    url: "/images/乃木コレ10.jpg",
+    price: 200,
   };
   const item5: ItemType = {
     id: 0,
-    name: "乃木コレ 五百城茉央",
-    status: "sold",
-    url: "/images/乃木コレ5_五百城.jpg",
-    price: 600,
+    name: "乃木コレ11",
+    status: "hold",
+    url: "/images/乃木コレ11.jpg",
+    price: 200,
   };
   const item6: ItemType = {
     id: 0,
-    name: "乃木コレ 伊藤理々杏SSR",
-    status: "sold",
-    url: "/images/乃木コレ6_伊藤SSR.jpg",
-    price: 3900,
+    name: "乃木コレ12",
+    status: "hold",
+    url: "/images/乃木コレ12.jpg",
+    price: 200,
   };
   const item7: ItemType = {
     id: 0,
-    name: "ポケモン ピカチュウ",
+    name: "ポケモン5",
     status: "hold",
-    url: "/images/ポケモン5_ピカチュウ昼寝",
-    price: 33000,
+    url: "/images/ポケモン5.jpg",
+    price: 200,
   };
   const item8: ItemType = {
     id: 0,
-    name: "ポケモン ナンジャモ",
+    name: "ポケモン6",
     status: "hold",
-    url: "/images/ポケモン8_ナンジャモR.jpg",
-    price: 89000,
+    url: "/images/ポケモン6.jpeg",
+    price: 200,
   };
   const item9: ItemType = {
     id: 0,
-    name: "ポケモン ナンジャモ",
+    name: "ポケモン7",
     status: "hold",
-    url: "/images/ポケモン7_ナンジャモ.jpg",
-    price: 67000,
+    url: "/images/ポケモン7.jpeg",
+    price: 200,
   };
   const item10: ItemType = {
     id: 0,
-    name: "ポケモン ナンジャモ",
+    name: "ポケモン8",
     status: "hold",
-    url: "/images/ポケモン8_ナンジャモR.jpg",
-    price: 89000,
+    url: "/images/ポケモン8.webp",
+    price: 200,
   };
   const item11: ItemType = {
     id: 0,
-    name: "ポケモン ミライドンex",
+    name: "ポケモン9",
     status: "hold",
-    url: "/images/ポケモン9_ミライドンex.jpg",
-    price: 3900,
+    url: "/images/ポケモン9.jpeg",
+    price: 200,
   };
   const item12: ItemType = {
     id: 0,
-    name: "ポケモン リザードンex2",
+    name: "ポケモン10",
     status: "hold",
-    url: "/images/ポケモン10_リザードンex2.jpg",
-    price: 2900,
+    url: "/images/ポケモン10.jpg",
+    price: 200,
   };
   return (
     <Layout title="home">
@@ -106,6 +145,17 @@ export default function Index() {
           </button>
         </div>
       </div>
+      {items !== undefined ? (
+        <div className="grid grid-cols-3 gap-4 p-2">
+          {items.map((item) => (
+            <button key={item.id} onClick={() => handleClick(item)}>
+              <Item item={item} showPrice={false} />
+            </button>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
       <div className="grid grid-cols-3 gap-4 p-2">
         <Item item={item6} showPrice={true} />
         <Item item={item5} showPrice={true} />
@@ -117,7 +167,7 @@ export default function Index() {
         <Item item={item8} showPrice={true} />
         <Item item={item2} showPrice={true} />
         <Item item={item12} showPrice={true} />
-        <Item item={item1} showPrice={true} />
+        <Item item={item} showPrice={true} />
         <Item item={item7} showPrice={true} />
       </div>
     </Layout>
--- frontend/pages/user.tsx
+++ frontend/pages/user.tsx
@@ -2,19 +2,62 @@ import Item from "@/components/items/Item";
 import Layout from "@/components/layout/Layout";
 import { ItemType } from "@/types/ItemType";
 import Link from "next/link";
-// import { ItemNC1 } from "@/components/items/ItemProducts";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setItem } from "../lib/features/item/itemSlice";
 
 export default function Index() {
+  // const uid = useSelector((state: any) => state.uid.value);
+  const user_data = useSelector((state: any) => state.user.value);
+  const [items, setItems] = useState<ItemType[]>();
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch(process.env.API_URL + "/items/user/" + `${user_data.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const itemsList: ItemType[] = data.map((item: any) => ({
+          id: item.id,
+          name: item.name,
+          status: item.status,
+          url: item.image,
+          price: item.desired_price,
+        }));
+        setItems(itemsList);
+        console.log(items);
+      });
+  }, []);
+
+  const handleClick = (item: ItemType) => {
+    try {
+      dispatch(setItem(item));
+
+      router.push("/item");
+    } catch (error) {
+      console.error("Error while fetching user data", error);
+    }
+  };
+
+  const item: ItemType = {
+    id: 0,
+    name: "乃木コレ 齋藤飛鳥SSR",
+    status: "",
+    url: "/images/乃木コレ_齋藤飛鳥SSR.jpg",
+    price: 21000,
+  };
   const item2: ItemType = {
     id: 0,
     name: "乃木コレ 賀喜遥香SSR",
     status: "",
-    url: "/images/乃木コレ3_賀喜遥香SSR.jpg",
+    url: "/images/乃木コレ1.jpg",
     price: 14000,
   };
   const item3: ItemType = {
     id: 0,
-    name: "乃木コレ 齋藤飛鳥SSR",
+    name: "乃木コレ 齋藤飛鳥SR",
     status: "",
     url: "/images/乃木コレ2.jpg",
     price: 7900,
@@ -23,63 +66,63 @@ export default function Index() {
     id: 0,
     name: "乃木コレ 中村麗乃",
     status: "",
-    url: "/images/乃木コレ4_中村.jpg",
+    url: "/images/乃木コレ3.jpg",
     price: 300,
   };
   const item5: ItemType = {
     id: 0,
     name: "乃木コレ 五百城茉央",
     status: "sold",
-    url: "/images/乃木コレ5_五百城.jpg",
+    url: "/images/乃木コレ4.jpeg",
     price: 600,
   };
   const item6: ItemType = {
     id: 0,
     name: "乃木コレ 伊藤理々杏SSR",
     status: "sold",
-    url: "/images/乃木コレ6_伊藤SSR.jpg",
+    url: "/images/乃木コレ5.jpg",
     price: 3900,
   };
   const item7: ItemType = {
     id: 0,
     name: "乃木コレ 賀喜遥香SSR",
     status: "",
-    url: "/images/乃木コレ7_賀喜SSR.jpg",
+    url: "/images/乃木コレ11.jpg",
     price: 14000,
   };
   const item8: ItemType = {
     id: 0,
     name: "乃木コレ 五百城茉央SR",
     status: "sold",
-    url: "/images/乃木コレ8_五百城SR.jpg",
+    url: "/images/乃木コレ7.jpg",
     price: 2200,
   };
   const item9: ItemType = {
     id: 0,
     name: "乃木コレ 池田瑛紗",
     status: "",
-    url: "/images/乃木コレ9_池田1.jpg",
+    url: "/images/乃木コレ8.jpg",
     price: 580,
   };
   const item10: ItemType = {
     id: 0,
     name: "乃木コレ 池田瑛紗",
     status: "sold",
-    url: "/images/乃木コレ10_池田2.jpg",
+    url: "/images/乃木コレ9.jpg",
     price: 580,
   };
   const item11: ItemType = {
     id: 0,
     name: "乃木コレ 池田瑛紗",
     status: "",
-    url: "/images/乃木コレ11_池田3.jpg",
+    url: "/images/乃木コレ10.jpg",
     price: 580,
   };
   const item12: ItemType = {
     id: 0,
     name: "乃木コレ 山下美月SSR",
     status: "",
-    url: "/images/乃木コレ12_山下SSR.jpg",
+    url: "/images/乃木コレ12.jpg",
     price: 16000,
   };
   return (
@@ -87,13 +130,13 @@ export default function Index() {
       <div className="h-20 flex items-center justify-center">
         <div className="mr-4">
           <img
-            src="/images/ユーザーアイコン.jpg"
+            src={`${user_data.icon}`}
             alt="ユーザーアイコン"
             className="w-12 h-12 rounded-full"
           />
         </div>
         <Link href="/user-assets" className="flex items-center">
-          <p className="text-4xl font-semibold">乃木坂コレクター</p>
+          <p className="text-4xl font-semibold">{`${user_data.name}`}</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="45"
@@ -110,7 +153,21 @@ export default function Index() {
           </svg>
         </Link>
       </div>
-      <div className="grid grid-cols-3 gap-4 p-2">
+      {/* <p>User ID: {uid}</p> */}
+      {items !== undefined ? (
+        <div className="grid grid-cols-3 gap-4 p-2">
+          {items.map((item) => (
+            <button key={item.id} onClick={() => handleClick(item)}>
+              <Item item={item} showPrice={false} />
+            </button>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+
+      {/* <div className="grid grid-cols-3 gap-4 p-2">
+        <Item item={item} showPrice={false} />
         <Item item={item2} showPrice={false} />
         <Item item={item3} showPrice={false} />
         <Item item={item4} showPrice={false} />
@@ -122,7 +179,7 @@ export default function Index() {
         <Item item={item10} showPrice={false} />
         <Item item={item11} showPrice={false} />
         <Item item={item12} showPrice={false} />
-      </div>
+      </div> */}
     </Layout>
   );
 }
